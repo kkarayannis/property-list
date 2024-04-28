bc14783952d2c9fa6baeb10f53913c577b38bc94
Kostas Karayannis <kkarayannis@gmail.com>
2024-04-28 10:29:25 +0200

    Add Xcode project

---
+++ PropertyList/PropertyList/PropertyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyListApp.swift
+//  PropertyList
+//
+//  Created by Konstadinos Karayannis on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PropertyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
